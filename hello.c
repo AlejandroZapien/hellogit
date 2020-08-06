72673147150ecb334370a5614a3ab290f37b4a9f
Alejandro <alejadnrozapien09@gmail.com>
2020-08-06 15:53:32 -0500

    oops forgot to add hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(int agrc, char * argv[]) {
+    int a = 3;
+    int b = 12;
+    int c = 0;
     printf("Here's some more code we are adding");
     printf("Hello git!\n");
+    printf("a = %d\nb = %d\nc = a + b = %d + %d = %d\n", a, b, a, b, a+b);
     return 0;
 }
